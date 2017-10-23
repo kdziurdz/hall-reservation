07e89fbff029917a461e95c548199d472f63371d
konrado <konrad.dziurdz@gmail.com>
2017-10-23 16:16:15 +0200

    initial commit

---
+++ src/main/java/pl/edu/pk/hallreservation/HallReservationApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.pk.hallreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HallReservationApplication.class, args);
+	}
+}
