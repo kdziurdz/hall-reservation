1807244f56bf0d7e0e169f954842581ae89ee9a9
konrado <konrad.dziurdz@gmail.com>
2017-10-25 17:25:26 +0200

    load data users

--- src/main/java/pl/edu/pk/hallreservation/model/User.java
+++ src/main/java/pl/edu/pk/hallreservation/model/User.java
@@ -23,6 +23,10 @@ public class User {
     @Column(name = "LAST_NAME")
     private String lastName;
 
+    @NotNull
+    @Column(name = "PASSWORD")
+    private String password;
+
     @Email
     @Column(name = "EMAIL")
     private String email;
@@ -64,6 +68,14 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
